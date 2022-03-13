5470f0b8f72fe50613fb5238e98ffb0326481c82
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-03-12 22:22:15 -0500

    refactor: created get collection functions

---
+++ opensea_wrapper/wrapper.py
@@ -0,0 +1,31 @@
+import requests
+import time
+
+base_url = 'https://api.opensea.io/api/v1'
+
+
+def get_collection(collection, api_key=None):
+    """Get collection from opensea
+
+    Args:
+        collection (str): collection name
+    """
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+
+    if api_key: headers = {'X-API-KEY': api_key}
+
+    url = f'{base_url}/{collection}'
+    response = None
+    repeat = True
+
+    while repeat:
+        response = requests.get(url, headers)
+
+        if response.status_code != 200:
+            time.sleep(1)
+            continue
+
+        response = response.json()
+        repeat = False
+
+    return response
